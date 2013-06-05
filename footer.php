5893f22aaef9e1b1e4ce4b67456cbef1ec1f4faf
Oscar Chinellato <oscar.chinellato@sickdevelopers.com>
2013-06-05 02:35:55 +0200

    Added mixins for rem font sizing. Modified CSS reset. Updated jquery to 1.10

--- footer.php
+++ footer.php
@@ -7,8 +7,8 @@
 </div> <!--! end of #container -->
 
 <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery.min.js"><\/script>')</script>
 
 <!-- scripts concatenated and minified via ant build script-->
 <script src="js/plugins.js"></script>
